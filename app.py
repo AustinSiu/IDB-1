998ccc3277ef195e80a11da7385d59873bc4e2d8
djpeacher <djpeacher@gmail.com>
2017-06-20 18:59:39 -0500

    Added search template

--- app.py
+++ app.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/bands')
+def bands():
+	return render_template('search.html', model=bands)
+
+
 app.run(debug=True)

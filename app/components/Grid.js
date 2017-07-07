3b02dd6772ea29dd99e780eb0fb348b41deaeaf6
Sarah Baxter <seb9000@sbcglobal.net>
2017-07-06 21:55:14 -0500

    adding Grid function for artist module

---
+++ app/components/Grid.js
@@ -0,0 +1,31 @@
+var React = require('react');
+var PropTypes = require('prop-types');
+
+
+function Grid(props) {
+  return(
+    <ul className="data-list">
+      {props.artists.map(function (artist) {
+        return (
+          <li key={artist.ArtistID} className='data-item'>
+            <ul className='data-list-items'>
+              <li>
+                <img
+                  className='avatar'
+                  src={artist.Image}
+                  alt={'Image for ' + artist.Name}/>
+              </li>
+              <li>{artist.Name}</li>
+            </ul>
+          </li>
+        )
+      })}
+    </ul>
+  )
+}
+
+Grid.propTypes = {
+  artists : PropTypes.array.isRequired,
+};
+
+module.exports = Grid;

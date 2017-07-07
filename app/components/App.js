77993d39cbc6718e981f4d9ef12efdbbdcd9bb32
Sarah Baxter <seb9000@sbcglobal.net>
2017-07-06 22:07:33 -0500

    adding react router initial files

--- app/components/App.js
+++ app/components/App.js
@@ -1,4 +1,10 @@
 var React = require('react');
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
+var Navbar = require('./Navbar');
+
 var Artists = require('./Artists');
 var Albums = require('./Albums');
 var Tours = require('./Tours');
@@ -8,9 +14,12 @@ var Songs = require('./Songs');
 class App extends React.Component {
   render() {
     return (
-      <div className='container'>
-      	<Artists />
-      </div>
+      <Router>
+        <div className='container'>
+        	<Artists />
+          <Route path='/artists' component={Artists} />
+        </div>
+      </Router>
     )
   }
 }

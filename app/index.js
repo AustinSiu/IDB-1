865cd5cc4f8ef71f09e3cd2b66795f000ef7b660
Sarah Baxter <seb9000@sbcglobal.net>
2017-07-11 01:32:48 -0500

    updated dependencies and run script

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 
-var App = require("./components/App")
+var App = require("./components/App");
 
 
 ReactDOM.render(

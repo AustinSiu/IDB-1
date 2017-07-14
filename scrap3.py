0a8af4f750ed3adb3c7715121623d1390fa188a4
djpeacher <djpeacher@gmail.com>
2017-07-14 15:33:28 -0500

    Updated data

--- scrap3.py
+++ scrap3.py
@@ -8,21 +8,16 @@ with open('data.json', 'r') as f:
 
 result = []
 for t in data['tours']:
-    s = data['tours'][t]['locations'].strip('.')
-    s = re.split(',', s)
-    s = ''.join(s)
-    s = s.replace('and ', '')
-    s = re.split(' ', s)
-    # for a in s:
-    #     b = a.replace('and ', '')
-    #     b = a.replace('and', '')
-    #     s.remove(a)
-    #     s += [b]
-    # if 'ect' in s:
-    #    s.remove('ect')
-    data['tours'][t]['locations'] = s
+    s = data['tours'][t]['venue']
+    # s = s.replace('Aftica', 'Africa')
+    s = s.replace(', ect.', '')
+    s = s.replace('.', '')
+    # s = ', '.join(s)
+    print(s)
 
-    print('%s' % (s))
+    data['tours'][t]['venue'] = s
+
+    # print('%s' % (s))
 
 # Writing JSON data
 with open('data.json', 'w') as f:
